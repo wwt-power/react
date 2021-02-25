da1f654d3cec41d94a8fe90b1f6526572a46ed70
286692865@qq.com <286692865@qq.com>
2021-02-25 10:07:51 +0800

    跨域配置

--- src/common/http/setupProxy.js
+++ src/common/http/setupProxy.js
@@ -1,14 +1,12 @@
-const proxy=require（http-proxy-middleware）：
+//const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app){
-	app.use(proxy("/devApi",{
+	app.use(createProxyMiddleware("/devApi",{
 		//配置要请求的服务器地址
 		target:"http://www.web-jshtml.cn/api/react",
-		changeOrigin:true
+		changeOrigin:true,
+		pathRewrite:{
+			"^/devApi":""
+		}
 	}))
-	
-	// app.use(proxy("/manage/api",{
-	// 	//配置要请求的服务器地址
-	// 	target:"http://",
-	// 	changeOrigin:true
-	// }))
 };
